5d67e1d7d2ee490916ce364a8b1ab53fd305f071
profanis <prodromouf@gmail.com>
2020-11-09 19:20:30 +0200

    lazy load components with condition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { LazyContentComponent } from './lazy-content/lazy-content.component';
 import { LazyWrapperComponent } from './lazy-wrapper/lazy-wrapper.component';
-import { Lazy1ContentComponent } from './lazy1-content/lazy1-content.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LazyContentComponent,
-    Lazy1ContentComponent,
     LazyWrapperComponent
   ],
   imports: [
@@ -18,9 +14,5 @@ import { Lazy1ContentComponent } from './lazy1-content/lazy1-content.component';
   ],
   providers: [],
   bootstrap: [AppComponent],
-  entryComponents: [
-    LazyContentComponent,
-    Lazy1ContentComponent
-  ]
 })
 export class AppModule { }
--- src/app/lazy-wrapper/lazy-wrapper.component.ts
+++ src/app/lazy-wrapper/lazy-wrapper.component.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactory, ComponentFactoryResolver, Input, OnChanges, ViewChild, ViewContainerRef } from '@angular/core';
+import { BaseContentComponent } from '../base-content.component';
 
 @Component({
   selector: 'app-lazy-wrapper',
   templateUrl: './lazy-wrapper.component.html',
   styleUrls: ['./lazy-wrapper.component.scss']
 })
-export class LazyWrapperComponent  {
+export class LazyWrapperComponent implements OnChanges  {
+  @ViewChild('lazyContent', { read: ViewContainerRef, static: true }) lazyContentContainer: ViewContainerRef
+
+  @Input() componentType: 'lazy' | 'lazy1'
+
+  constructor(private cfr: ComponentFactoryResolver) {
+  }
+
+  async ngOnChanges() {
+    let componentFactory: ComponentFactory<BaseContentComponent>
+
+    // lazy load component
+    if (this.componentType==='lazy') {
+      const lazyContentComponent = await import(`../lazy-content/lazy-content.component`)
+      componentFactory = this.cfr.resolveComponentFactory(lazyContentComponent.LazyContentComponent)
+    }
+
+    // lazy load component
+    if (this.componentType==='lazy1') {
+      const lazy1ContentComponent = await import(`../lazy1-content/lazy1-content.component`)
+      componentFactory = this.cfr.resolveComponentFactory(lazy1ContentComponent.Lazy1ContentComponent)
+    }
+
+    this.lazyContentContainer.clear()
+    this.lazyContentContainer.createComponent(componentFactory)
+  }
+
+
 }
