95d6e5b41a6d8e4a448148dd6ff3566ca9ef0e0a
profanis <prodromouf@gmail.com>
2020-11-14 19:35:10 +0200

    truly lazy load components with a pattern

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { LazyContentComponent } from './lazy-content/lazy-content.component';
 import { LazyWrapperComponent } from './lazy-wrapper/lazy-wrapper.component';
-import { Lazy1ContentComponent } from './lazy1-content/lazy1-content.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LazyWrapperComponent,
-    LazyContentComponent,
-    Lazy1ContentComponent,
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent],
-  entryComponents: [
-    LazyContentComponent,
-    Lazy1ContentComponent,
-  ]
 })
 export class AppModule { }
