3fce768cea0096e5a1a71b6a6f17727282872518
profanis <prodromouf@gmail.com>
2020-11-22 20:27:53 +0200

    use titleCasePipe

--- src/app/lazy-wrapper/lazy-wrapper.component.ts
+++ src/app/lazy-wrapper/lazy-wrapper.component.ts
@@ -1,16 +1,18 @@
+import { TitleCasePipe } from '@angular/common';
 import { Component, ComponentFactoryResolver, Input, OnChanges, ViewChild, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-lazy-wrapper',
   templateUrl: './lazy-wrapper.component.html',
-  styleUrls: ['./lazy-wrapper.component.scss']
+  styleUrls: ['./lazy-wrapper.component.scss'],
+  providers: [TitleCasePipe]
 })
 export class LazyWrapperComponent implements OnChanges  {
   @ViewChild('lazyContent', { read: ViewContainerRef, static: true }) lazyContentContainer: ViewContainerRef
 
   @Input() componentType: 'lazy' | 'lazy1'
 
-  constructor(private cfr: ComponentFactoryResolver) {
+  constructor(private cfr: ComponentFactoryResolver, private titleCasePipe: TitleCasePipe) {
   }
 
   async ngOnChanges() {
@@ -19,14 +21,9 @@ export class LazyWrapperComponent implements OnChanges  {
     }
 
     const lazyContentComponent = await import(`../${this.componentType}-content/${this.componentType}-content.component`)
-    const componentClassName = lazyContentComponent[`${this.capitalize(this.componentType)}ContentComponent`]
+    const componentClassName = lazyContentComponent[`${this.titleCasePipe.transform(this.componentType)}ContentComponent`]
     const componentFactory = this.cfr.resolveComponentFactory(componentClassName)
     this.lazyContentContainer.clear()
     this.lazyContentContainer.createComponent(componentFactory)
   }
-
-  private capitalize(value: string): string {
-    return `${value.charAt(0).toUpperCase()}${value.slice(1).toLowerCase()}`
-  }
-
 }
