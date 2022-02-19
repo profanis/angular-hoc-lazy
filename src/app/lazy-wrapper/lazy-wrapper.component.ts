0e245c1bfd52f3af6d2c76d97bae237128e537b3
profanis <prodromouf@gmail.com>
2022-02-19 10:12:30 +0200

    upgrade to NG 13

--- src/app/lazy-wrapper/lazy-wrapper.component.ts
+++ src/app/lazy-wrapper/lazy-wrapper.component.ts
@@ -1,41 +1,48 @@
-import { Component, ComponentFactory, ComponentFactoryResolver, Input, OnChanges, ViewChild, ViewContainerRef } from '@angular/core';
-import { BaseContentComponent } from '../base-content.component';
+import {
+  Component,
+  Input,
+  OnChanges,
+  Type,
+  ViewChild,
+  ViewContainerRef,
+} from "@angular/core";
+import { BaseContentComponent } from "../base-content.component";
 
 @Component({
-  selector: 'app-lazy-wrapper',
-  templateUrl: './lazy-wrapper.component.html',
-  styleUrls: ['./lazy-wrapper.component.scss']
+  selector: "app-lazy-wrapper",
+  templateUrl: "./lazy-wrapper.component.html",
+  styleUrls: ["./lazy-wrapper.component.scss"],
 })
-export class LazyWrapperComponent implements OnChanges  {
-  @ViewChild('lazyContent', { read: ViewContainerRef, static: true }) lazyContentContainer: ViewContainerRef
+export class LazyWrapperComponent implements OnChanges {
+  @ViewChild("lazyContent", { read: ViewContainerRef, static: true })
+  lazyContentContainer: ViewContainerRef;
 
-  @Input() componentType: 'lazy' | 'lazy1'
-
-  constructor(private cfr: ComponentFactoryResolver) {
-  }
+  @Input() componentType: "lazy" | "lazy1";
 
   async ngOnChanges() {
     if (!this.componentType) {
-      return
+      return;
     }
 
-    let componentFactory: ComponentFactory<BaseContentComponent>
+    let componentTypeToLoad: Type<BaseContentComponent>;
 
     // lazy load component
-    if (this.componentType === 'lazy') {
-      const lazyContentComponent = await import(`../lazy-content/lazy-content.component`)
-      componentFactory = this.cfr.resolveComponentFactory(lazyContentComponent.LazyContentComponent)
+    if (this.componentType === "lazy") {
+      const lazyContentComponent = await import(
+        `../lazy-content/lazy-content.component`
+      );
+      componentTypeToLoad = lazyContentComponent.LazyContentComponent;
     }
 
     // lazy load component
-    if (this.componentType === 'lazy1') {
-      const lazy1ContentComponent = await import(`../lazy1-content/lazy1-content.component`)
-      componentFactory = this.cfr.resolveComponentFactory(lazy1ContentComponent.Lazy1ContentComponent)
+    if (this.componentType === "lazy1") {
+      const lazy1ContentComponent = await import(
+        `../lazy1-content/lazy1-content.component`
+      );
+      componentTypeToLoad = lazy1ContentComponent.Lazy1ContentComponent;
     }
 
-    this.lazyContentContainer.clear()
-    this.lazyContentContainer.createComponent(componentFactory)
+    this.lazyContentContainer.clear();
+    this.lazyContentContainer.createComponent(componentTypeToLoad);
   }
-
-
 }
